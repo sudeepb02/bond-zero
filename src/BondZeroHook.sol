9d579214b4171914e8f4851a177b3964c6f041d5
Sudeep Biswas <sudeepb02@gmail.com>
2025-09-28 00:11:40 +0530

    chore: remove unused hooks

--- src/BondZeroHook.sol
+++ src/BondZeroHook.sol
@@ -18,77 +18,20 @@ contract BondZeroHook is BaseHook {
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
             beforeInitialize: false,
-            afterInitialize: true,
-            beforeAddLiquidity: true,
-            afterAddLiquidity: true,
-            beforeRemoveLiquidity: true,
-            afterRemoveLiquidity: true,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
             beforeSwap: true,
-            afterSwap: true,
+            afterSwap: false,
             beforeDonate: false,
             afterDonate: false,
-            beforeSwapReturnDelta: false,
+            beforeSwapReturnDelta: true,
             afterSwapReturnDelta: false,
             afterAddLiquidityReturnDelta: false,
             afterRemoveLiquidityReturnDelta: false
         });
-    }
-
-    ////////////////////////////////////////////////////////
-    ///////////////// Initialization Hooks /////////////////
-    ////////////////////////////////////////////////////////
-
-    function afterInitialize(address sender, PoolKey calldata key, uint160 sqrtPriceX96, int24 tick)
-        external
-        override
-        onlyPoolManager
-        returns (bytes4)
-    {
-        return BaseHook.afterInitialize.selector;
-    }
-
-    ////////////////////////////////////////////////////////
-    //////////////////// Liquidity Hooks ///////////////////
-    ////////////////////////////////////////////////////////
-
-    /// @dev The Math logic and state updates before liquidity is added and LP tokens are minted
-    function _beforeAddLiquidity(
-        address sender,
-        PoolKey calldata key,
-        ModifyLiquidityParams calldata params,
-        bytes calldata hookData
-    ) internal override returns (bytes4) {
-        return BaseHook.beforeAddLiquidity.selector;
-    }
-
-    function _afterAddLiquidity(
-        address,
-        PoolKey calldata,
-        ModifyLiquidityParams calldata,
-        BalanceDelta,
-        BalanceDelta,
-        bytes calldata
-    ) internal override returns (bytes4, BalanceDelta) {
-        return (BaseHook.afterAddLiquidity.selector, BalanceDeltaLibrary.ZERO_DELTA);
-    }
-
-    function _beforeRemoveLiquidity(address, PoolKey calldata key, ModifyLiquidityParams calldata, bytes calldata)
-        internal
-        override
-        returns (bytes4)
-    {
-        return BaseHook.beforeRemoveLiquidity.selector;
-    }
-
-    function _afterRemoveLiquidity(
-        address,
-        PoolKey calldata,
-        ModifyLiquidityParams calldata,
-        BalanceDelta,
-        BalanceDelta,
-        bytes calldata
-    ) internal override returns (bytes4, BalanceDelta) {
-        return (BaseHook.afterRemoveLiquidity.selector, BalanceDeltaLibrary.ZERO_DELTA);
     }
 
     ////////////////////////////////////////////////////////
@@ -101,13 +44,5 @@ contract BondZeroHook is BaseHook {
         returns (bytes4, BeforeSwapDelta, uint24)
     {
         return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
-    }
-
-    function _afterSwap(address, PoolKey calldata key, SwapParams calldata, BalanceDelta, bytes calldata)
-        internal
-        override
-        returns (bytes4, int128)
-    {
-        return (BaseHook.afterSwap.selector, 0);
     }
 }
